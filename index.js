119f9aba3e991886fde9e2e62fd6bf14130316f6
Gousheng777 <70049668+Gousheng777@users.noreply.github.com>
2024-04-23 01:46:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ handlers.makeAPICall = function (args, context) {
     var request = {
         PlayFabId: currentPlayerId, Statistics: [{
                 StatisticName: "Level",
-                Value: 5
+                Value: 9
             }]
     };
     // The pre-defined "server" object has functions corresponding to each PlayFab server API 
